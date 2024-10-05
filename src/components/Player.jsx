e478590dc26c2b00904117dcd950df2bc5111732
De-DavicDesigner <buztech20@gmail.com>
2024-10-05 12:20:31 +0100

    Update App,GameBoard file and Include GameOver and Player files

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react"
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
 
     const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+        
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
     }
 
     function handlePlayerName(event) {
@@ -16,7 +20,9 @@ export default function Player({initialName, symbol, isActive}) {
     return (
         <li className={isActive ? 'active' : undefined}>
             <span className="player">
-                {isEditing ? <input type="text" required value={playerName} onChange={handlePlayerName}></input> : <span className="player-name">{playerName}</span> }
+                {isEditing ? <input type="text" required value={playerName} onChange={handlePlayerName}></input> : 
+                    <span className="player-name">{playerName}</span> 
+                }
                 <span className="player-symbol">{symbol}</span>
             </span>
             <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
